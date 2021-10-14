46e7052f86c71dad36bdcc74aa5caf5f0b8ed482
Holly Teunis <+>
2021-10-13 22:04:11 -0400

    commit tree ed4e506c1b3c634d75d8416e5cff408749064fad

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code here
+function saturdayFun (b){
+    b = (typeof b !== 'undefined') ?  b : "roller-skate"
+    return `This Saturday, I want to ${b}!`
+
+}
+
+
+    const mondayWork = function(b) {
+        b = (typeof b !== 'undefined') ?  b : "go to the office"
+          return `This Monday, I will ${b}.`
+
+    }
+
+    function wrapAdjective(work = "*") {
+        return function (part1 = "tight") {
+            return `You are ${work}${part1}${work}!`; 
+        }
+            
+    }
+  
